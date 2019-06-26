7133c1f54172e44a887320a2c5b7ef3bdb2b1b52
vipparty <vipmanoj09@gmail.com>
2019-06-25 20:13:03 -0400

    Manoj - Updated icons list with my fav icon

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -8,16 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ListPage implements OnInit {
   private selectedItem: any;
   private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
+    'body'
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
   constructor() {
